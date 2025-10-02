5e9a024dec2be84323924854d47f0d19d185230d
kylelovestoad <kylelovestoad+github@protonmail.com>
2025-10-02 04:20:04 -0400

    Add small test websocket code that functions like a chat

--- src/ws.rs
+++ src/ws.rs
@@ -1,26 +1,41 @@
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
 use axum::extract::{State, WebSocketUpgrade};
-use axum::extract::ws::WebSocket;
+use axum::extract::ws::{Message, WebSocket};
+use futures_util::{SinkExt, StreamExt};
+use mongodb::bson::Uuid;
+use rand::random;
 use crate::AppState;
 
 pub async fn ws_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> impl axum_core::response::IntoResponse {
     ws.on_upgrade(|socket| handle_socket(socket, state))
 }
 
-async fn handle_socket(mut socket: WebSocket, app_state: Arc<AppState>) {
-    while let Some(msg) = socket.recv().await {
-        let msg = if let Ok(msg) = msg {
-            msg
-        } else {
-            // Client disconnected
-            return;
-        };
+async fn handle_socket(mut socket: WebSocket, state: Arc<AppState>) {
 
-        // Process the message (e.g., echo it back, broadcast to other clients)
-        if socket.send(msg).await.is_err() {
-            // Client disconnected
-            return;
+    let (mut sender, mut receiver) = socket.split();
+
+    let mut rx = state.tx.subscribe();
+
+    let rand: i64 = random();
+    let msg = format!("{rand} joined.");
+    tracing::debug!("{msg}");
+    let _ = state.tx.send(msg);
+
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            // In any websocket error, break loop.
+            if sender.send(Message::text(msg)).await.is_err() {
+                break;
+            }
         }
-    }
+    });
+
+    let tx = state.tx.clone();
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(text))) = receiver.next().await {
+            let _ = tx.send(format!("{text}"));
+        }
+    });
 }
